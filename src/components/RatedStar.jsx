82f88260d566eb8c06a9e749637c29fac60691d6
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-04-07 21:22:19 +0100

    top rating fix

--- src/components/RatedStar.jsx
+++ src/components/RatedStar.jsx
@@ -2,37 +2,13 @@ import React from 'react'
 
 const RatedStar = ({ rating }) => {
   const stars = [];
+  
   for (let i = 0; i < 5; i++) {
-    stars.push(<span key={i} className={i <= rating ? 'filled' : 'empty'}>★</span>);
+    stars.push(<span key={i} className={i < Math.floor(rating) ? 'filled' : 'empty'}>★</span>);
   }
   return (
     <div>{stars}</div>
   )
-  // const starPercentage = (rating / 5) * 100;
-
-  // // Round to the nearest 10 to get a whole or half star rating
-  // const starRounded = `${Math.round(starPercentage / 10) * 10}%`;
-
-  // return (
-  //   <div className="star-rating">
-  //     <div className="star-rating-front" style={{ width: starRounded }}>
-  //       {/* Render the star icons */}
-  //       <span className="fa fa-star"></span>
-  //       <span className="fa fa-star"></span>
-  //       <span className="fa fa-star"></span>
-  //       <span className="fa fa-star"></span>
-  //       <span className="fa fa-star"></span>
-  //     </div>
-  //     <div className="star-rating-back">
-  //       {/* Render the greyed-out star icons */}
-  //       <span className="fa fa-star"></span>
-  //       <span className="fa fa-star"></span>
-  //       <span className="fa fa-star"></span>
-  //       <span className="fa fa-star"></span>
-  //       <span className="fa fa-star"></span>
-  //     </div>
-  //   </div>
-  // )
 }
 
 export default RatedStar
